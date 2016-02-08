892b36142cbafaeb2fa959e31dca1783842be1ca
Tobias <qenzoz@gmail.com>
2016-02-08 16:21:14 +0100

    Spawn enemy

--- js/app.js
+++ js/app.js
@@ -8,10 +8,15 @@ var PLAYER_MOVE_UP = -100;
 var PLAYER_MOVE_RIGHT = 100;
 var PLAYER_MOVE_DOWN = 100;
 
+var ENEMY_START_X_LOCATION = -100;
+var ENEMY_START_Y_LOCATION_TOP = 50;
+var ENEMY_START_Y_LOCATION_BOTTOM = 210;
+
 var KEYCODE_LEFT = "left";
 var KEYCODE_UP = "up";
 var KEYCODE_RIGHT = "right";
 var KEYCODE_DOWN = "down";
+
 
 // Enemies our player must avoid
 var Enemy = function () {
@@ -21,6 +26,8 @@ var Enemy = function () {
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
+  this.x = 0;
+  this.y = Math.floor(Math.random() * ((ENEMY_START_Y_LOCATION_BOTTOM - ENEMY_START_Y_LOCATION_TOP) + 1)) + ENEMY_START_Y_LOCATION_TOP;
 };
 
 // Update the enemy's position, required method for game
@@ -70,7 +77,7 @@ Player.prototype.checkWallCollision = function () {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-allEnemies = [];
+allEnemies = [new Enemy()];
 
 var player = new Player();
 
